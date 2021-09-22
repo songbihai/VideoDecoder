e1f7397354fbde40d367629b71fdf05b819f144d
songbihai <>
2021-09-22 17:35:34 +0800

    carthage build

---
+++ Example/Pods/Target Support Files/Pods-VideoDecoder-Example-VideoDecoder-ExampleUITests/Pods-VideoDecoder-Example-VideoDecoder-ExampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VideoDecoder_Example_VideoDecoder_ExampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_VideoDecoder_Example_VideoDecoder_ExampleUITests
+@end
---
+++ Example/Pods/Target Support Files/VideoDecoder/VideoDecoder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_VideoDecoder : NSObject
+@end
+@implementation PodsDummy_VideoDecoder
+@end
